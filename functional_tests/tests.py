d8af278ac14ee7012d262081ed31fdec31ee64b6
Sana256 <sana256@yandex.ru>
2018-01-03 15:56:29 +0300

    "new URL + view for adding to existing lists. FT passes)"

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
+from lists.models import Item
+
 
 MAX_WAIT = 5
 
@@ -27,9 +29,9 @@ class NewVisitorTest(LiveServerTestCase):
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
-                time.sleep(1)
+                time.sleep(0.5)
 
-    """def test_can_start_a_list_for_one_user(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
 
@@ -48,7 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock to make a fly')"""
+        self.wait_for_row_in_list_table('2: Use peacock to make a fly')
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
@@ -70,13 +72,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
+
         self.wait_for_row_in_list_table('1: Buy milk')
 
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
-        page_text = self.browser.find_element_by_id('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock fearther', page_text)
         self.assertIn('Buy milk', page_text)
 
